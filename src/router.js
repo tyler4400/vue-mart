9fc64b126355b21d3090295362fce5361e430f9c
tyler4400 <tyler4400@foxmail.com>
2019-10-04 16:50:08 +0800

    完成登录表单及登录逻辑的控制

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import Home from './views/Home.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
@@ -14,13 +14,36 @@ export default new Router({
             component: Home
         },
         {
+            path: '/login',
+            name: 'login',
+            component: () => import('@/views/Login')
+        },
+        {
             path: '/about',
             name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
+            meta: {
+                auth: true
+            },
             component: () =>
                 import(/* webpackChunkName: "about" */ './views/About.vue')
         }
     ]
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.auth) {
+        const token = sessionStorage.getItem('vue-mart-token');
+        if (token) {
+            next();
+        } else {
+            next({
+                path: '/login',
+                query: { redirect: to.path }
+            });
+        }
+    } else {
+        next();
+    }
+});
+
+export default router;
--- src/store.js
+++ src/store.js
@@ -1,10 +1,36 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import us from '@/service/user';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {}
+    state: {
+        isLogin: Boolean(sessionStorage.getItem('vue-mart-token'))
+    },
+    mutations: {
+        setLoginState(state, b) {
+            state.isLogin = b;
+        }
+    },
+    actions: {
+        login({ commit }, user) {
+            // 登录请求
+            return us.login(user).then(res => {
+                const { code, token } = res.data;
+                if (code) {
+                    // 登录成功
+                    commit('setLoginState', true);
+                    sessionStorage.setItem('vue-mart-token', token);
+                }
+                return code;
+            });
+        },
+        logout({ commit }) {
+            // 清缓存
+            sessionStorage.removeItem('vue-mart-token');
+            // 重置状态
+            commit('setLoginState', false);
+        }
+    }
 });
